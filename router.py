108c2412fa18e8b5f48c32833698e5cfb5de35d6
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-06-06 15:10:19 +0530

    Add changes

--- router.py
+++ router.py
@@ -1,7 +1,13 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, jsonify
 import geoip2.database
+import json
+
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+
 
 ip_address_map = {
     'LK': '20.51.237.16',
@@ -26,35 +32,52 @@ def register_endpoint():
     return 'IP address registered successfully', 200
 
 # Load the GeoIP2 database
-geoip_database = geoip2.database.Reader('/home/azureuser/GeoLite2-City.mmdb')
+geoip_database = geoip2.database.Reader('/home/nuvidu/fyp/GeoLite2-City.mmdb')
 
 @app.route('/publish', methods=['POST'])
 def original_endpoint1():
-    # Process the incoming data if needed
-
     # Get the user's IP address from the request
-    user_ip = request.remote_addr
-    # user_ip="103.1.176.0"
-    # user_ip="175.157.74.223"
-    print("ip: ",user_ip)
+    # user_ip = request.remote_addr
+    user_ip = "175.157.74.223"
+    print("ip: ", user_ip)
 
     try:
         # Get the user's location based on the IP address
         response = geoip_database.city(user_ip)
-        # print("res: ",response)
         location = response.country.iso_code
-        # print("loc: ",location)
     except geoip2.errors.AddressNotFoundError:
         # Handle the case when the IP address is not found in the GeoIP database
         return 'Location not found', 404
 
     # Retrieve the corresponding IP address from the hashmap based on the location
     new_ip_address = ip_address_map.get(location)
-    # print("new: ",new_ip_address)
+
     if new_ip_address:
-        # Perform the redirect
-        new_url = f'http://{new_ip_address}:8081/publish'
-        return redirect(new_url, code=308)
+        # Construct the payload
+        payload = {
+            "query": "select order.orderId@string, order.country@string, item.unitPrice@float, order.totalRevenue@float, order.totalCost@float, order.totalProfit@float, order.eventTimestamp@long from item join order on item.itemType@string=order.itemType@string",
+            "apiKey": "Tu_TZ0W2cR92-sr1j-l7ACA.newone.9pej9tihskpx2vYZaxubGW3sFCJLzxe55NRh7T0uk1JMYiRmHdiQsWh5JhRXXT6c418385",
+            "id": "ZXCVB",
+            "locationEnabled": True
+        }
+
+        # Create the response object
+        response = redirect(f'http://{new_ip_address}:8081/publish', code=308)
+
+        # if request.method == 'OPTIONS':
+        #     response.headers['Access-Control-Request-Method'] = 'POST'
+
+        # Set the Access-Control-Allow-Origin header to allow requests from the same origin
+        response.headers['Access-Control-Allow-Origin'] = '*'  # or specify the appropriate origin
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
+        response.headers['Content-Type'] = 'application/json'
+
+        # Set the Payload header in the redirect response
+        response.headers['Payload'] = json.dumps(payload)
+
+        return response
     else:
         # Handle the case when the location is not found in the hashmap
         return 'Location not found', 404
@@ -64,7 +87,8 @@ def original_endpoint2():
     # Process the incoming data if needed
 
     # Get the user's IP address from the request
-    user_ip = request.remote_addr
+    # user_ip = request.remote_addr
+    user_ip="175.157.74.223"
 
     try:
         # Get the user's location based on the IP address
@@ -80,7 +104,12 @@ def original_endpoint2():
     if new_ip_address:
         # Perform the redirect
         new_url = f'http://{new_ip_address}:8081/orderInfo'
-        return redirect(new_url, code=308)
+        response = redirect(new_url, code=308)
+
+        # Set the Access-Control-Allow-Origin header to allow requests from the same origin
+        response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin')
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        return response
     else:
         # Handle the case when the location is not found in the hashmap
         return 'Location not found', 404
